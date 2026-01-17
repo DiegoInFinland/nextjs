8f81dd7eea3d99d964c2ccf6cc044c3ce23e2fc4
DiegoInFinland <dmartin.falco@gmail.com>
2026-01-17 13:19:05 +0200

    Added icons and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+/* 
+CDN daisyui styles:
+<link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+*/
+
 import ButtonAdd from "./components/Button";
 
 export default function Home() {
